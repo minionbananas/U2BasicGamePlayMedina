af368d9b7221d3273eb299e81e6b720eb6309c4d
Anthonyggs <anthony102210@icloud.com>
2025-03-24 10:22:24 -0700

    wrote script
    wrote the player controller script

--- BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+public float horizontalInput;
+public float speed = 10.0f;
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
     }
 }
