3a1b8b5875158f5d89175d98ae73ef8f08f149a1
Anthonyggs <anthony102210@icloud.com>
2025-03-26 09:59:07 -0700

    wrote script
    worked on script

--- BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+{ 
 public float horizontalInput;
 public float speed = 10.0f;
-{
+public float xRange = 10.0f;
+
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
