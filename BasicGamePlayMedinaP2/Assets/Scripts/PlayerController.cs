3df65a553178c50688138390c60ff71dd9fa2848
Anthonyggs <anthony102210@icloud.com>
2025-03-28 09:58:01 -0700

    sctipt
    wrote script

--- BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayMedinaP2/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,15 @@ public float xRange = 10.0f;
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
